a27d399fc1988f92661bcbc4d745ad9c542faefc
RickyAlma <jerrydandi@gmail.com>
2026-05-19 23:49:42 +0100

    Create Docker Compose.yml

---
+++ docker-stack/frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>React Frontend</h1>
+      <p>Connected to Flask API</p>
+    </div>
+  );
+}
+
+export default App;
